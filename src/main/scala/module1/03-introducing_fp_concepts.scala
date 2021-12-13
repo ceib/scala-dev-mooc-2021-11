cdc3958f19e4f540d36a6e7ff28341c74936491b
vorontsovas <vorontsovas@rainvest.ru>
2021-12-13 17:30:41 +0300

    Homework1
    Signed-off-by: vorontsovas <vorontsovas@rainvest.ru>

--- src/main/scala/module1/03-introducing_fp_concepts.scala
+++ src/main/scala/module1/03-introducing_fp_concepts.scala
@@ -224,36 +224,31 @@ object hof{
            case Option.Some(v) => f(v)
            case Option.None => Option.None
        }
+
+      /** Реализовать метод printIfAny, который будет печатать значение, если оно есть */
+       def printIfAny(): Unit = this match {
+           case Option.Some(v) => println(v)
+           case Option.None =>
+       }
+
+      /** Реализовать метод zip, который будет создавать Option от пары значений из 2-х Option */
+       def zip[B](anotherOption: Option[B]): Option[(T, B)] = {
+         if (isEmpty || anotherOption.isEmpty) Option.None else Option.Some(this.get, anotherOption.get)
+       }
+
+      /** Реализовать метод filter, который будет возвращать не пустой Option в случае если исходный не пуст и предикат от значения = true */
+       def filter(f: T => Boolean): Option[T] = this match {
+         case Option.Some(v) => if (f(v)) this else Option.None
+         case Option.None => Option.None
+       }
    }
 
    object Option{
         case class Some[T](v: T) extends Option[T]
         case object None extends Option[Nothing]
 
-        def apply[T](v: T): Option[T] = ???
+        def apply[T](v: T): Option[T] = if (v != null) Some(v) else None
    }
-
-
-
-
-
-  /**
-   *
-   * Реализовать метод printIfAny, который будет печатать значение, если оно есть
-   */
-
-
-  /**
-   *
-   * Реализовать метод zip, который будет создавать Option от пары значений из 2-х Option
-   */
-
-
-  /**
-   *
-   * Реализовать метод filter, который будет возвращать не пустой Option
-   * в случае если исходный не пуст и предикат от значения = true
-   */
 
  }
 
@@ -266,59 +261,114 @@ object hof{
     * Cons - непустой, содердит первый элемент (голову) и хвост (оставшийся список)
     */
 
-    sealed trait List[+T]
+    sealed trait List[+T] {
 
-    case class ::[A](head: A, tail: List[A]) extends List[A]
-    case object Nil extends List[Nothing]
+     import List._
 
-    
+     /** Метод cons, добавляет элемент в голову списка, для этого метода можно воспользоваться названием `::` */
+     def ::[A >: T](newHead: A): List[A] = this match {
+       case list: ::[T] => List.::(newHead, list)
+       case Nil => List.::(newHead, Nil)
+     }
 
+     /** Метод cons, добавляет элемент в голову списка, для этого метода можно воспользоваться названием `::` */
+     def cons[A >: T](newHead: A): List[A] = newHead :: this
 
-    /**
-     * Метод cons, добавляет элемент в голову списка, для этого метода можно воспользоваться названием `::`
-     *
+     /** Метод mkString возвращает строковое представление списка, с учетом переданного разделителя */
+     def mkString(delimiter: String): String = this match {
+       case ::(head, tail) =>
+         @tailrec
+         def loop(list: List[T], string: String): String = list match {
+           case ::(head_, tail_) => loop(tail_, string + delimiter + head_.toString)
+           case Nil => string
+         }
+         loop(tail, head.toString)
+       case Nil => EmptyString
+     }
+
+     /** Реализовать метод reverse который позволит заменить порядок элементов в списке на противоположный */
+     def reverse: List[T] = this match {
+       case l: ::[T] =>
+         @tailrec
+         def loop(list : List[T], newList: List[T]): List[T] = list match {
+           case ::(head, tail) => loop(tail, head :: newList)
+           case Nil => newList
+         }
+         loop(this, Nil)
+       case Nil => Nil
+     }
+
+     /** Реализовать метод map для списка который будет применять некую ф-цию к элементам данного списка
+       *
+       * <p>Реализовано с помощью хвостовой рекурсии, но подобный подход требует вызова .reverse на результирующем списке для сохранения последовательности элементов исходного списка.
+       * С точки зрения алгоритмизации лучше использовать формирование списка в цикле с использованием локальных var. Также можно использовать рекурсию не хвостовую, но
+       * в этом случае ограничиваем возможные размеры обрабатываемого списка (из-за конечности стека)</p>
+       * */
+     def map[A](f: T => A): List[A] = this match {
+       case list: ::[T] =>
+         @tailrec
+         def loop(listT: List[T], listA: List[A]): List[A] = listT match {
+           case ::(head, tail) => loop(tail, f(head) :: listA)
+           case Nil => listA
+         }
+         loop(list, Nil).reverse
+       case Nil => Nil
+     }
+
+     /** Реализовать метод filter для списка который будет фильтровать список по некому условию
+       *
+       * <p>Реализовано с помощью хвостовой рекурсии, но подобный подход требует вызова .reverse на результирующем списке для сохранения последовательности элементов исходного списка.
+       * С точки зрения алгоритмизации лучше использовать формирование списка в цикле с использованием локальных var. Также можно использовать рекурсию не хвостовую, но
+       * в этом случае ограничиваем возможные размеры обрабатываемого списка (из-за конечности стека)</p>
+       */
+     def filter(f: T => Boolean): List[T] = this match {
+       case list: ::[T] =>
+         @tailrec
+         def loop(listT: List[T], newList: List[T]): List[T] = listT match {
+           case ::(head, tail) =>
+             if (f(head)) loop(tail, head :: newList) else loop(tail, newList)
+           case List.Nil => newList
+         }
+         loop(this, Nil).reverse
+       case Nil => Nil
+     }
+
+   }
+
+   object List {
+
+     final val EmptyString: String = ""
+
+     case class ::[A](head: A, tail: List[A]) extends List[A]
+     case object Nil extends List[Nothing]
+
+     /**
+       * Конструктор, позволяющий создать список из N - го числа аргументов
+       * Для этого можно воспользоваться *
+       *
+       * Например вот этот метод принимает некую последовательность аргументов с типом Int и выводит их на печать
+       * def printArgs(args: Int*) = args.foreach(println(_))
+       */
+     def apply[A](elements: A*): List[A] = {
+       @tailrec
+       def loop(list: List[A], seq: scala.collection.Seq[A]): List[A] = {
+         if (seq.isEmpty) list else loop(seq.head :: list, seq.tail)
+       }
+       if (elements.nonEmpty) {
+         loop(Nil, elements).reverse
+       } else Nil
+     }
+
+   }
+
+   /** Написать функцию incList котрая будет принимать список Int и возвращать список,
+     * где каждый элемент будет увеличен на 1
      */
+   def incList(list: List[Int]): List[Int] = list.map(_ + 1)
 
-    /**
-      * Метод mkString возвращает строковое представление списка, с учетом переданного разделителя
-      *
-      */
-
-    /**
-      * Конструктор, позволяющий создать список из N - го числа аргументов
-      * Для этого можно воспользоваться *
-      * 
-      * Например вот этот метод принимает некую последовательность аргументов с типом Int и выводит их на печать
-      * def printArgs(args: Int*) = args.foreach(println(_))
-      */
-
-    /**
-      *
-      * Реализовать метод reverse который позволит заменить порядок элементов в списке на противоположный
-      */
-
-    /**
-      *
-      * Реализовать метод map для списка который будет применять некую ф-цию к элементам данного списка
-      */
-
-
-    /**
-      *
-      * Реализовать метод filter для списка который будет фильтровать список по некому условию
-      */
-
-    /**
-      *
-      * Написать функцию incList котрая будет принимать список Int и возвращать список,
-      * где каждый элемент будет увеличен на 1
-      */
-
-
-    /**
-      *
-      * Написать функцию shoutString котрая будет принимать список String и возвращать список,
-      * где к каждому элементу будет добавлен префикс в виде '!'
-      */
+   /** Написать функцию shoutString котрая будет принимать список String и возвращать список,
+     * где к каждому элементу будет добавлен префикс в виде '!'
+     */
+   def shoutString(listOfStrings: List[String]): List[String] = listOfStrings.map(str => if (str != null) "!" + str else "!" )
 
  }
