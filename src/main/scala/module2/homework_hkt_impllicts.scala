09e32e7ab076d5497437bdfd38ee39a71a84ab87
Aleksey Vorontsov <avorontsov1987@gmail.com>
2022-01-09 17:26:03 +0300

    - Реализация метода tupleF с использованием неявным преобразований к HKT Bindable
    Signed-off-by: Aleksey Vorontsov <avorontsov1987@gmail.com>

--- src/main/scala/module2/homework_hkt_impllicts.scala
+++ src/main/scala/module2/homework_hkt_impllicts.scala
@@ -1,5 +1,7 @@
 package module2
 
+import scala.language.implicitConversions
+
 object homework_hkt_impllicts{
 
     /**
@@ -10,14 +12,24 @@ object homework_hkt_impllicts{
       *   val r2 = println(tupleF(list1, list2))
       * 
       */
-    def tupleF[F[_], A, B](fa: F[A], fb: F[B]) = ???
+    def tupleF[F[_], A, B](fa: Bindable[F, A], fb: Bindable[F, B]): F[(A, B)] =
+      fa.flatMap(a => fb.map(b => (a, b) ))
 
 
-    trait Bindable[F[_], A] {
+  trait Bindable[F[_], A] {
         def map[B](f: A => B): F[B]
         def flatMap[B](f: A => F[B]): F[B]
     }
 
+  implicit def bindableOpt[A](opt: Option[A]): Bindable[Option, A] = new Bindable[Option, A] {
+    override def map[B](f: A => B): Option[B] = opt.map(f)
+    override def flatMap[B](f: A => Option[B]): Option[B] = opt.flatMap(f)
+  }
+
+  implicit def bindableList[A](list: List[A]): Bindable[List, A] = new Bindable[List, A] {
+    override def map[B](f: A => B): List[B] = list.map(f)
+    override def flatMap[B](f: A => List[B]): List[B] = list.flatMap(f)
+  }
 
   val optA: Option[Int] = Some(1)
   val optB: Option[Int] = Some(2)
@@ -27,4 +39,5 @@ object homework_hkt_impllicts{
 
   val r1 = println(tupleF(optA, optB))
   val r2 = println(tupleF(list1, list2))
+
 }
