7d1d623b96188e187ef077890c5c9d532eef76cd
Ivan Lukashchuk <i.lukashchuk@perenio.com>
2022-05-18 13:00:51 +0300

    + Higher kind types, implicits, type classes, часть 1

--- src/main/scala/module2/homework_hkt_impllicts.scala
+++ src/main/scala/module2/homework_hkt_impllicts.scala
@@ -10,13 +10,30 @@ object homework_hkt_impllicts{
       *   val r2 = println(tupleF(list1, list2))
       * 
       */
-    def tupleF[F[_], A, B](fa: F[A], fb: F[B]) = ???
+    def tupleF[F[_], A, B](fa: Bindable[F, A], fb: Bindable[F, B]): F[(A, B)] =
+      fa.flatMap(a => fb.map(b => (a,b)))
 
 
     trait Bindable[F[_], A] {
         def map[B](f: A => B): F[B]
         def flatMap[B](f: A => F[B]): F[B]
     }
+
+  implicit def optToBindable[A](option: Option[A]): Bindable[Option, A] = {
+    new Bindable[Option, A] {
+      override def map[B](f: A => B): Option[B] = option.map(f)
+
+      override def flatMap[B](f: A => Option[B]): Option[B] = option.flatMap(f)
+    }
+  }
+
+  implicit def listToBindable[A](list: List[A]): Bindable[List, A] = {
+    new Bindable[List, A] {
+      override def map[B](f: A => B): List[B] = list.map(f)
+
+      override def flatMap[B](f: A => List[B]): List[B] = list.flatMap(f)
+    }
+  }
 
 
   val optA: Option[Int] = Some(1)
